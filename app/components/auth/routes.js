4874fd833d487c8f4eba58610373b7cf5b724169
haiha04 <hanong0410@gmail.com>
2022-12-07 23:48:49 +0700

    Initial Project

---
+++ app/components/auth/routes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const AuthMiddleware = require('@auth/middlewares/AuthMiddleware');
+
+const LoginController = require('./controllers/LoginController');
+
+router.post('/login', (req, res, next) => LoginController.login(req, res));
+
+router.post('/reset-token', AuthMiddleware.authenticated, (req, res, next) => LoginController.resetToken(req, res));
+
+module.exports = router;
